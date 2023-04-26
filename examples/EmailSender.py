beb868d6429e2bbc814cb731eaeb71dc1f58a1e1
Shawn Armstrong <smarmstr@ucsc.edu>
2023-04-26 08:17:36 -0700

    Resolve changes

--- examples/EmailSender.py
+++ examples/EmailSender.py
@@ -3,11 +3,11 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 class EmailSender:
-    def __init__(self, smtp_host, smtp_port, username, password, recipients):
+    def __init__(self, smtp_host, smtp_port, username, token_password, recipients):
         self.smtp_host = smtp_host
         self.smtp_port = smtp_port
         self.username = username
-        self.password = password
+        self.token_password = token_password
         self.recipients = recipients
 
     def create_message(self, subject, html_content):
@@ -22,7 +22,7 @@ class EmailSender:
         try:
             with smtplib.SMTP(self.smtp_host, self.smtp_port) as server:
                 server.starttls()
-                server.login(self.username, self.password)
+                server.login(self.username, self.token_password)
                 server.sendmail(self.username, self.recipients, message.as_string())
                 print("Email sent successfully")
         except smtplib.SMTPException as e:
--- examples/record.py
+++ examples/record.py
@@ -29,21 +29,19 @@ import sys
 sys.path.append("..")
 import rtde.rtde as rtde
 import rtde.rtde_config as rtde_config
-import rtde.csv_writer as csv_writer
-import rtde.csv_binary_writer as csv_binary_writer
 import EmailSender as es
 import datetime
 
-max_messages = 1
-current_messages = 0
-
+# Put your information here; this is all you need to do.
+# Check email-sender README.md.
 email_sender = es.EmailSender(
     smtp_host="smtp.gmail.com",
     smtp_port=587,
     username='<SENDER_EMAIL>@gmail.com',
-    password='<YOUR_APP_PASSWORD>', # NOT your ordinary sign-in password; look at email-sender README.md.
+    token_password='<YOUR_APP_PASSWORD>', # NOT your ordinary sign-in password; look at email-sender README.md.
     recipients=["recipient2@example.com", "recipient2@example.com"]
 )
+
 
 def safety_status_bits_to_dict(value):
     
@@ -68,10 +66,28 @@ def safety_status_bits_to_dict(value):
     return safety_dict
 
 def safety_dict_to_html(safety_dict):
+
     html_lines = []
+
     for key, value in safety_dict.items():
         html_lines.append(f"<p>{key}: {value}</p>")
     return "\n".join(html_lines)
+
+
+def send_safety_notification(safety_data, email_sender):
+    safety_dict_html = safety_dict_to_html(safety_data)
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    message = email_sender.create_message(
+                        subject=f"RTDE NOTIFICATION: {current_time}", 
+                        html_content= f'''
+                        <h1>ROBOT RTDE WARNING</h1>
+                        <p>A safety related stop has occurred.
+                        {safety_dict_html}</p>
+                        ''')
+    # email_sender.send_email(message)
+
+    print("Notification sent.")
 
 
 # parameters
@@ -81,20 +97,12 @@ parser.add_argument(
 )
 parser.add_argument("--port", type=int, default=30004, help="port number (30004)")
 parser.add_argument(
-    "--samples", type=int, default=0, help="number of samples to record"
-)
-parser.add_argument(
     "--frequency", type=int, default=125, help="the sampling frequency in Herz"
 )
 parser.add_argument(
     "--config",
     default="record_configuration.xml",
     help="data configuration file to use (record_configuration.xml)",
-)
-parser.add_argument(
-    "--output",
-    default="robot_data.csv",
-    help="data output file to write to (robot_data.csv)",
 )
 parser.add_argument("--verbose", help="increase output verbosity", action="store_true")
 parser.add_argument(
@@ -129,64 +137,28 @@ if not con.send_start():
     logging.error("Unable to start synchronization")
     sys.exit()
 
-writeModes = "wb" if args.binary else "w"
-with open(args.output, writeModes) as csvfile:
-    writer = None
+is_running = True
+while is_running:
 
-    if args.binary:
-        writer = csv_binary_writer.CSVBinaryWriter(csvfile, output_names, output_types)
-    else:
-        writer = csv_writer.CSVWriter(csvfile, output_names, output_types)
+    try:
+        if args.buffered:
+            state = con.receive_buffered(args.binary)
+        else:
+            state = con.receive(args.binary)
 
-    writer.writeheader()
+        # Extract safety bits
+        safety_data = safety_status_bits_to_dict(state.safety_status_bits)
+        
+        # Ensure robot is nominal; otherwise, send email.
+        if safety_data["Is normal mode"] == False:
+            send_safety_notification(safety_data, email_sender)
+            sys.exit() 
 
-    i = 1
-    keep_running = True
-    while keep_running:
-
-        if i % args.frequency == 0:
-            if args.samples > 0:
-                sys.stdout.write("\r")
-                sys.stdout.write("{:.2%} done.".format(float(i) / float(args.samples)))
-                sys.stdout.flush()
-            else:
-                sys.stdout.write("\r")
-                sys.stdout.write("{:3d} samples.".format(i))
-                sys.stdout.flush()
-        if args.samples > 0 and i >= args.samples:
-            keep_running = False
-        try:
-            if args.buffered:
-                state = con.receive_buffered(args.binary)
-            else:
-                state = con.receive(args.binary)
-
-            safety_data = safety_status_bits_to_dict(state.safety_status_bits)
-            
-            if safety_data["Is normal mode"] == False and current_messages < max_messages:
-                safety_dict_html = safety_dict_to_html(safety_data)
-                current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                message = email_sender.create_message(
-                                    subject=f"RTDE NOTIFICATION: {current_time}", 
-                                    html_content= f'''
-                                    <h1>ROBOT RTDE WARNING</h1>
-                                    <p>A safety related stop has occurred.
-                                    {safety_dict_html}</p>
-                                    ''')
-                email_sender.send_email(message)
-                print("Notification sent.")
-                current_messages += 1
-                i += 1
-                sys.exit() 
-
-        except KeyboardInterrupt:
-            keep_running = False
-        except rtde.RTDEException:
-            con.disconnect()
-            sys.exit()
-
-
-sys.stdout.write("\rComplete!            \n")
+    except KeyboardInterrupt:
+        is_running = False
+    except rtde.RTDEException:
+        con.disconnect()
+        sys.exit()
 
 con.send_pause()
 con.disconnect()
